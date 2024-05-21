5add853290273113b45395a784549997d1b461a0
AV-Prabu-17 <avprabu02@gmail.com>
2024-05-21 18:21:03 +0530

    New  updates

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    baseUrl:'https://rahulshettyacademy.com/angularpractice',
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      
+    },
+  },
+});
+
+module.exports = {
+  projectId: "prmd8a",
+  // ...rest of the Cypress project config
+}
