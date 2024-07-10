f83c256212e93293c3b74616fdd0a20f791d6741
AV-Prabu-17 <avprabu02@gmail.com>
2024-07-10 12:02:19 +0530

    updating the changes to branch

--- cypress.config.js
+++ cypress.config.js
@@ -3,18 +3,10 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    baseUrl:'https://rahulshettyacademy.com/angularpractice',
-     setupNodeEvents(on, config) {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-      
-    },
-  },
+  }
+  
 });
-
-module.exports = {
-  projectId: "prmd8a",
-  // ...rest of the Cypress project config
-}
+  
